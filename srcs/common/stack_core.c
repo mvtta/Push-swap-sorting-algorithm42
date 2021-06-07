fc802792f665f3ce2af1e5afe55bc7c609e842da
If you're my friend you know my name <70669920+m4r11@users.noreply.github.com>
2021-06-07 20:56:43 +0100

    dll finally dominated lol

--- srcs/common/stack_core.c
+++ srcs/common/stack_core.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stack_core.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvaldeta <user@student.42lisboa.com>       +#+  +:+       +#+        */
+/*   By: user <mvaldeta@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 16:14:15 by mvaldeta          #+#    #+#             */
-/*   Updated: 2021/06/04 19:29:34 by mvaldeta         ###   ########.fr       */
+/*   Updated: 2021/06/07 20:24:40 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include "pslib.h"
 
-t_stack_link 	*new_link(int data, t_stack_info *stack)
+t_stack_link *new_link(int data, t_stack_info *stack)
 {
 	t_stack_link *new;
 	new = malloc(sizeof(t_stack_link));
@@ -25,61 +25,52 @@ t_stack_link 	*new_link(int data, t_stack_info *stack)
 	new->next = NULL;
 	stack->size++;
 
-	return(new);
+	return (new);
 }
 
-t_stack_info	*new_stack(void)
+t_stack_info *new_stack(void)
 {
-	t_stack_info	*new;
+	t_stack_info *new;
 
 	new = malloc(sizeof(t_stack_info));
 	new->head = NULL;
 	new->tail = NULL;
 	new->size = 0;
 
-	return(new);
+	return (new);
 }
 
-static inline t_stack_link  **find_link(t_stack_info *stack, t_stack_link *target)
+static inline t_stack_link **find_link(t_stack_info *stack, t_stack_link *target)
 {
 	t_stack_link **p = &stack->head;
-	
-	while((*p) && (*p) != target)
+
+	while ((*p) && (*p) != target)
 		p = &(*p)->next;
 	return p;
 }
 
-void			stack_pop_front(t_stack_info *stack, t_stack_link *out)
+void stack_pop_front(t_stack_info *stack)
 {
-	t_stack_link **p;
+	t_stack_link *p;
 
-	*p = out->next;
-	stack->head = (*p);
-	free(&out);
+	p = stack->head;
+	stack->head = stack->head->next;
+	free(p);
 	stack->size--;
-	
 }
 
-void			stack_pop_back(t_stack_info *stack, t_stack_link *out)
+void stack_pop_back(t_stack_info *stack)
 {
-	t_stack_link **p;
 	t_stack_link *to_del;
-	t_stack_link *newtail;
 
-	printf("node received : %d\n", out->value);
 	to_del = stack->tail;
 	stack->tail = stack->tail->prev;
-	newtail = stack->tail->prev;
-	printf("last : %d", to_del->value);
-	printf("new last : %d", newtail->value);
-	//free(to_del);
-/* 	free(out->prev);
-	free(out->next);
-	free(&out->value); */
+	free(to_del);
+	printf("new last : %p\n", stack->tail);
 	stack->size--;
 }
 
-void			stack_key_pop_push(t_stack_info *stack, t_stack_link *target)
+void stack_key_pop_push(t_stack_info *stack, t_stack_link *target)
 {
 	t_stack_link **p = find_link(stack, target);
 	*p = target->next;
@@ -87,64 +78,79 @@ void			stack_key_pop_push(t_stack_info *stack, t_stack_link *target)
 	stack->size--;
 }
 
-int	stack_top_peek(t_stack_info *stack)
+int stack_top_peek(t_stack_info *stack)
 {
-		return(stack->head->value);
-
+	return (stack->head->value);
 }
-int	stack_tail_peek(t_stack_info *stack)
+int stack_tail_peek(t_stack_info *stack)
 {
-		return(stack->tail->value);
+	return (stack->tail->value);
 }
 
-t_stack_link			*stack_append(t_stack_info *stack, t_stack_link *new, int i)
+t_stack_link *stack_append(t_stack_info *stack, t_stack_link *new, int i)
 {
 	t_stack_link *to_insert;
 	to_insert = new_link(i, stack);
 	to_insert->prev = stack->tail;
 	stack->tail = to_insert;
 
-	return(to_insert);
+	return (to_insert);
 }
 
-int		main(void)
+int find_value(t_stack_link *value, int the_one)
+{
+	while (value->value != the_one)
+		value = value->next;
+	return (value->value);
+}
+
+int main(void)
 {
 	t_stack_info *stack = NULL;
 	t_stack_link *element = NULL;
 	int i = 0;
 
 	stack = new_stack();
-	element =  new_link(i, stack);
+	element = new_link(i, stack);
 	stack->head = element;
 	//printf("%d\n", element->value);
-	while(i++ <= 10)
+	while (i++ <= 10)
 	{
 		element->next = stack_append(stack, element, i);
 		element = element->next;
 	}
- 	element = stack->head;
-	while(element->next != NULL)
-	{
-		printf("%d\n", element->value);
-		printf("next :%p\n", element->next);
-		printf("node :%p\n\n", element);
-		element = element->next;
-	}
-	stack_pop_back(stack, element);
-	printf("head:%d\n", stack->head->value);
-	printf("peek head:%d\n", stack_top_peek(stack));
-	printf("tail:%d\n", stack->tail->value);
-	printf("peek tail:%d\n", stack_tail_peek(stack));
-/* 	element = stack->head;
-	stack_pop_back(stack, element+=1);
-	while(element->next != NULL)
+	element = stack->head;
+	/* 	while(element->next != NULL)
 	{
 		printf("%d\n", element->value);
 		printf("next :%p\n", element->next);
 		printf("node :%p\n\n", element);
 		element = element->next;
 	} */
-/* 	printf("head:%d", stack->head->value);
-	printf("tail:%d", stack->tail->value); */
-
+	while (element != stack->tail)
+	{
+		printf("%d\n", element->value);
+		printf("next :%p\n", element->next);
+		printf("node :%p\n\n", element);
+		element = element->next;
+	}
+	printf("size before pop: %d\n", stack->size);
+	stack_pop_back(stack);
+	printf("size after pop: %d\n", stack->size);
+	printf("size before pop2: %d\n", stack->size);
+	stack_pop_back(stack);
+	printf("size after pop2: %d\n", stack->size);
+	element = stack->head;
+	printf("find value: %d\n", find_value(element, 7));
+	element = stack->head;
+	stack_pop_front(stack);
+	printf("size after pop-front: %d\n", stack->size);
+	element = stack->head;
+	while (element != stack->tail)
+	{
+		printf("%d\n", element->value);
+		printf("next :%p\n", element->next);
+		printf("node :%p\n\n", element);
+		element = element->next;
+	}
 }
